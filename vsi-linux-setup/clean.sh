86169fd20f1fc5661316f329617680b87d17deca
Juan Pinzon <juancpinzone@hotmail.com>
2020-03-27 16:35:16 -0700

    Update setup of IBM packer plugin on Ubuntu-based VSI

--- vsi-linux-setup/clean.sh
+++ vsi-linux-setup/clean.sh
@@ -1,8 +1,9 @@
 source stuff
 echo "$red Deleting ALL"
-rm -rf $HOME/ibmcloud/$go_tar
-rm -rf $HOME/ibmcloud/$packer_zip
 rm -rf $HOME/go
+rm -rf $HOME/temp/$go_tar
+rm -rf $HOME/temp/$packer_zip
 rm -rf /usr/local/go
 rm -rf /usr/local/packer
-rm -rf $HOME/.ssh
+source /etc/environment
+echo $PATH
