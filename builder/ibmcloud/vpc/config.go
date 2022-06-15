14078c7af1de77ec955a51a31980fd8654a3a3c7
astha.jain <Astha.Jain@ibm.com>
2022-06-14 22:09:39 -0400

    Update user data file read optional

--- builder/ibmcloud/vpc/config.go
+++ builder/ibmcloud/vpc/config.go
@@ -72,16 +72,6 @@ func (c *Config) Prepare(raws ...interface{}) ([]string, error) {
 		errs = packer.MultiErrorAppend(errs, errors.New("a region must be specified"))
 	}
 
-	// Configure IBM Cloud EndPoint and other IBM Cloud API constants
-	c.EndPoint = "https://" + c.Region + ".iaas.cloud.ibm.com/v1/"
-	// According with the IBM Cloud - VPC API documentation (https://cloud.ibm.com/apidocs/vpc#api-versioning),
-	// we should be hard-coding the version and test the plugin with new API versions, to maintain ongoing compatibility.
-	// c.Version = "version=2021-10-15"
-	c.Version = fmt.Sprintf("version=%d-%02d-%02d", currentTime.Year(), currentTime.Month(), currentTime.Day())
-
-	c.Generation = "generation=2"
-	// log.Println("Version : ", c.Version)
-
 	if c.SubnetID == "" {
 		errs = packer.MultiErrorAppend(errs, errors.New("a subnet_id must be specified"))
 	}
