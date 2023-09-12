6db7cbc4b34a07a69f0e77a2c246e5fa296ea885
Astha Jain <astha.jain@ibm.com>
2023-09-12 14:40:58 -0400

    Update version.go
    Signed-off-by: Astha Jain <astha.jain@ibm.com>

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ var IBMCloudPluginVersion *version.PluginVersion
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version           = "v3.2.1"
+	Version           = "v3.2.2"
 	VersionPrerelease = "dev"
 )
 
