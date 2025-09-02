8c794d39dc27d9afda53676699a1e26f5d214825
Ujjwal Kumar <78945437+uibm@users.noreply.github.com>
2025-09-02 10:18:54 +0530

    feat(release): updated version to v3.3.2 (#136)
    Signed-off-by: Ujjwal Kumar <ujjwal.kumar1@ibm.com>

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ var IBMCloudPluginVersion *version.PluginVersion
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version           = "v3.3.1"
+	Version           = "v3.3.2"
 	VersionPrerelease = "dev"
 )
 
