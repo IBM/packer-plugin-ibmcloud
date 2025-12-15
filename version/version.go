d49d2eb2999f0f2454962bd8e1dd66dc08d4e762
Ujjwal Kumar <ujjwal.kumar1@ibm.com>
2025-12-15 13:29:11 +0530

    updated version
    Signed-off-by: Ujjwal Kumar <ujjwal.kumar1@ibm.com>

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ var IBMCloudPluginVersion *version.PluginVersion
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version           = "v3.3.3"
+	Version           = "v3.3.4"
 	VersionPrerelease = "dev"
 )
 
